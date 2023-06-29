d5c1a32b82f12ad88aef2f42556fb599a2a444e3
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2023-06-28 17:31:25 -0700

    :tada: Create Capability and Permission Types

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::marker::PhantomData;
+use std::path::PathBuf;
+
+pub struct Read;
+pub struct Write;
+pub struct Copy;
+pub struct Move;
+pub struct Delete;
+pub struct NotGranted;
+
+// Provides capability safe wrapper for PathBuf with A, B, C, D, and E representing Read,
+// Write, Copy, Move, and Delete respectively.
+pub struct Capability<A, B, C, D, E> {
+    path: PathBuf,
+    phantom: PhantomData<(A, B, C, D, E)>,
+}
+
+// Implements methods for Capabilities with any permissions
+impl<A, B, C, D, E> Capability<A, B, C, D, E> {
+    pub fn new(path_string: &str) -> Capability<A, B, C, D, E> {
+        Capability {
+            path: PathBuf::from(path_string),
+            phantom: PhantomData::<(A, B, C, D, E)>
+        }
+    }
+}
+
