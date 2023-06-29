4db0d42bca61bc125aa85552c486ff6a03f83316
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2023-06-28 17:39:01 -0700

    :bulb: Add Example of Manual Capability Creation

---
+++ examples/manual_capability.rs
@@ -0,0 +1,6 @@
+use coenobita::{ Capability, Read, Copy, Move, NotGranted };
+
+fn main() {
+    // Manually creates an instance of Capability with read, copy, and move permissions
+    let rcm_cap: Capability<Read, NotGranted, Copy, Move, NotGranted> = Capability::new("example.txt");
+}
