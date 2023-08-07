948f1b24ed63f30005c20a0656900a2fe4e672c6
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2023-08-07 10:02:56 -0700

    :wrench: Fix Directory Impls. and dir! Macro

--- examples/directories.rs
+++ examples/directories.rs
@@ -4,4 +4,6 @@ use coenobita::fs;
 fn main() {
     let dir_cap = cap!("examples/files" with Read, Delete);
     let dir_cap = dir!(dir_cap with Read, Delete);
+
+    println!("{:?}", dir_cap);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -21,11 +21,13 @@ pub struct Delete;
 
 // Provides capability safe wrapper for PathBuf with A, B, C, D, and E representing Read,
 // Write, Copy, Move, and Delete respectively.
+#[derive(Debug)]
 pub struct Capability<A, B, C, D, E> {
     path: PathBuf,
     phantom: PhantomData<(A, B, C, D, E)>,
 }
 
+#[derive(Debug)]
 pub struct Directory<A, B> {
     path: PathBuf,
     phantom: PhantomData<(A, B)>
@@ -47,17 +49,25 @@ impl<A, B, C, D, E> Capability<A, B, C, D, E> {
 }
 
 impl<A, B, C, D, E, F, G, H, I, J> Directory<Capability<A, B, C, D, E>, Capability<F, G, H, I, J>> {
+    pub fn new(path_string: &str) -> Directory<Capability<A, B, C, D, E>, Capability<F, G, H, I, J>> {
+        Directory {
+            path: PathBuf::from(path_string),
+            phantom: PhantomData::<(Capability<A, B, C, D, E>, Capability<F, G, H, I, J>)>
+        }
+    }
+
     // Path must have a getter because it's private - programmers shouldn't be able to change it
     pub fn get_path(&self) -> &PathBuf {
         &self.path
     }
 }
 
-impl<A, B, C, D, E, F> From<Capability<A, B, C, D, E>> for Directory<F, Capability<A, B, C, D, E>> {
-    fn from(value: Capability<A, B, C, D, E>) -> Directory<F, Capability<A, B, C, D, E>> {
-        Directory::<F, Capability::<A, B, C, D, E>> {
-            path: value.get_path().to_path_buf(),
-            phantom: PhantomData::<(F, Capability<A, B, C, D, E>)>
+impl<A> Directory<A, ()> {
+    pub fn from<B, C, D, E, F>
+    (cap: Capability<B, C, D, E, F>) -> Directory<A, Capability<B, C, D, E, F>> {
+        Directory {
+            path: cap.get_path().to_path_buf(),
+            phantom: PhantomData::<(A, Capability<B, C, D, E, F>)>
         }
     }
 }
